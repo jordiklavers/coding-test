48fd60c1e2312161da60d09c84286f1345ee0ed0
Jordi Klavers <jordiklavers@gmail.com>
2025-02-27 19:07:35 +0100

    checkpoint 1

---
+++ src/helpers/reveal.js
@@ -0,0 +1,9 @@
+export function reveal() {
+    const startHiddenAttributeName = "start-hidden"
+    const elements = document.querySelectorAll(`[${startHiddenAttributeName}]`);
+
+    elements.forEach(element => {
+        element.removeAttribute(startHiddenAttributeName);
+    });
+}
+
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import { Clock } from './clock';
+import { reveal } from './helpers/reveal';
+
+const clock = new Clock('h1');
+
+reveal();
